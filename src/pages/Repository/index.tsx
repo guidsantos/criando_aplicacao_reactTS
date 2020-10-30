3b8a3f8d14c64b153dc42dadeaf76c5922642d10
guidsantos <guilherme.fast2503@gmail.com>
2020-10-29 22:28:50 -0300

    Estilizando Detalhe

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { useRouteMatch, Link } from 'react-router-dom';
+
+import logoImg from '../../assets/logoGitExplorer.png';
+
+import { Header, Issues, RepositoryInfo } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -9,10 +14,52 @@ const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
   return (
-    <h1>
-      Repository:
-      {params.repository}
-    </h1>
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHub Explorer" />
+        <Link to="/dashboard">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars3.githubusercontent.com/u/55068041?s=460&u=306c25cafa897092d4a0483d561ff980fb81d109&v=4"
+            alt="Avatar"
+          />
+          <div>
+            <strong>guidsantos/database-upload</strong>
+            <p>descrição do repo</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1208</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1128</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="asashuhuas">
+          <div>
+            <strong>ashuashuas</strong>
+            <p>ashuuashuhashuas</p>
+          </div>
+
+          <FiChevronRight size={40} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
